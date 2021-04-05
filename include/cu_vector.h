1bbbff62da11159aafad3d90e6fd6a36c77a5019
William Koch <47284092+wak31415@users.noreply.github.com>
2021-04-05 15:02:59 +0200

    Fix issues with cuda vector class
    Also removes standard vector class entirely

--- include/cu_vector.h
+++ include/cu_vector.h
@@ -5,10 +5,16 @@ class CU_Vector {
     public:
         __host__ __device__ CU_Vector() : N(VecSize) {
             memset(data, 0, sizeof(data));
-        };
+        }
 
-        __host__ __device__ CU_Vector(float* data, int N) : N(VecSize) {
-            this->data = data;
+        __host__ __device__ CU_Vector(float* data) : N(VecSize) {
+            memcpy(this->data, data, VecSize*sizeof(float));
+        }
+
+        __host__ __device__ CU_Vector(float x, float y, float z) : N(3) {
+            data[0] = x;
+            data[1] = y;
+            data[2] = z;
         }
 
         __host__ __device__ int get_size() { return N; }
@@ -16,6 +22,8 @@ class CU_Vector {
         __host__ __device__ const float& operator[](int i) const { return data[i]; }
         __host__ __device__ float& operator[](int i) { return data[i]; }
 
+
+        // Defining Math Operators
         __host__ __device__ CU_Vector& operator+=(const CU_Vector& b) {
             for(int i=0; i<N; i++) {
                 data[i] += b[i];
@@ -23,11 +31,24 @@ class CU_Vector {
             return *this;
         }
 
-        template <class S>
-        __host__ __device__ CU_Vector& operator*=(const S& scalar) {
+        __host__ __device__ CU_Vector& operator-=(const CU_Vector& b) {
+            for(int i=0; i<N; i++) {
+                data[i] -= b[i];
+            }
+            return *this;
+        }
+
+        template <class T>
+        __host__ __device__ CU_Vector& operator*=(const T& scalar) {
             for(int i=0; i<N; i++) {
                 data[i] *= scalar;
             }
+            return *this;
+        }
+
+        template <class T>
+        __host__ __device__ CU_Vector& operator/=(const T& scalar) {
+            this *= 1.f/scalar;
             return *this;
         }
 
@@ -40,28 +61,53 @@ class CU_Vector {
             return sqrtf(_norm);
         }
 
+        __host__ __device__ void normalize() {
+            const float _norm = this->norm();
+            for (int i = 0; i < N; i++)
+                data[i] /= _norm;
+        }
+
     protected:
         int N;
         float data[VecSize];
 };
 
-class CU_Vector3f : public CU_Vector<3> 
-{
-    using CU_Vector::CU_Vector;
+typedef CU_Vector<3> CU_Vector3f;
 
-    public:
-        __host__ __device__ CU_Vector3f(float x, float y, float z) {
-            data[0] = x;
-            data[1] = y;
-            data[2] = z;
-        }
-};
 
-template <class T>
-__host__ __device__ T operator+(T &a, T &b) {
-    T result;
+// Overload vector operations
+template <class Vec>
+__host__ __device__ Vec operator+(Vec a, Vec b) {
+    Vec result;
     for(int i = 0; i < a.get_size(); i++) {
         result[i] = a[i] + b[i];
     }
     return result;
-}
+};
+
+template <int VecSize, class T>
+__host__ __device__ CU_Vector<VecSize> operator*(const T &scalar, const CU_Vector<VecSize> &v) {
+    float data[VecSize];
+    for (int i = 0; i < VecSize; i++)
+        data[i] = scalar * v[i];
+    
+    return CU_Vector<VecSize>(data);
+};
+
+template <int VecSize, class T>
+__host__ __device__ CU_Vector<VecSize> operator*(const CU_Vector<VecSize> &v, const T &scalar) {
+    return scalar * v;
+};
+
+
+// Define dot and cross products
+template <class Vec>
+__host__ __device__ float dot(const Vec& a, const Vec& b) {
+    float dot_prod = 0.f;
+    for(int i = 0; i < a.get_size(); i++) dot_prod += a[i]*b[i];
+    return dot_prod;
+};
+
+__host__ __device__ CU_Vector<3> cross(const CU_Vector<3> &a, const CU_Vector<3> &b) {
+    return CU_Vector<3>(a[1]*b[2]-a[2]*b[1], a[2]*b[0]-a[0]*b[2], a[0]*b[1]-a[1]*b[0]);
+};
--- src/main.cu
+++ src/main.cu
@@ -7,9 +7,9 @@ int write_image();
 __global__ void vector_add_kernel(CU_Vector3f *c) {
     int i = threadIdx.x;
     if (i < N) {
-        CU_Vector3f a({3.f, 2.f, 1.f});
+        CU_Vector3f a({9.f, 6.f, 3.f});
 
-        c[i] = a + c[i];
+        c[i] = a + 3*c[i];
     }
 }
 
@@ -19,10 +19,10 @@ int main() {
 
     CU_Vector3f c[N];
     for(int i=0; i<N; i++) {
-        c[i] = CU_Vector3f({1.f, 2.f, 3.f});
+        c[i] = CU_Vector3f(1.f, 2.f, 3.f);
     }
 
-    int size = sizeof(CU_Vector3f) * N;
+    int size = sizeof(CU_Vector<3>) * N;
 
     CU_Vector3f *d_c;
     cudaMalloc((void**)&d_c, size);
@@ -36,7 +36,7 @@ int main() {
     for(int i=0; i<10; i++) {
         printf("Value of c: {%.1f, %.1f, %.1f}\n", c[i][0], c[i][1], c[i][2]);
         for(int j=0; j<3; j++) {
-            if (c[i][j] != 4.f) {
+            if (c[i][j] != 12.f) {
                 printf("Error: incorrect value (%.1f)\n", c[i][j]);
             }
         }
